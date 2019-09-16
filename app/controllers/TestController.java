fbe114d44e2210fe6764827eef56d3cf3ccb7943
mathewjpallan <mathewjpallan@gmail.com>
2019-09-16 14:31:37 +0530

    Working play2.5 app

--- app/controllers/TestController.java
+++ app/controllers/TestController.java
@@ -1,19 +1,9 @@
 package controllers;
 
-import com.typesafe.config.Config;
 import play.mvc.Controller;
-import play.mvc.Http;
 import play.mvc.Result;
 
-import javax.inject.Inject;
-
 public class TestController extends Controller {
-    private final Config config;
-
-    @Inject
-    public TestController(Config config) {
-        this.config = config;
-    }
 
     public Result testApi() {
          return ok("works");
